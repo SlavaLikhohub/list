237cecbb6ec5422f2d8a51a1af2b8882117a9aef
Viacheslav <s.likhogub@gmail.com>
2020-08-31 09:24:02 +0300

    Update Makefile

--- Makefile
+++ Makefile
@@ -3,8 +3,9 @@ AR ?= $(PREF)ar
 CC = $(PREF)gcc
 PREFIX ?= /usr/local
 
-CFLAGS = -O2 -Wall -Wextra -Ideps
-CFLAGS +=-mcpu=cortex-m4 -mthumb -mfpu=fpv4-sp-d16 -mfloat-abi=hard --static -nostartfiles -lnosys -Lbuild/release -Wl,--start-group -lc -lgcc -Wl,--end-group -labst_stm32f4 -lopencm3 -llist -Wl,--gc-sections -Wl,--print-gc-sections -specs=nano.specs -specs=nosys.specs
+CFLAGS ?= #Should be given by previous level
+#CFLAGS = -O2 -Wall -Wextra -Ideps
+#CFLAGS +=-mcpu=cortex-m4 -mthumb -mfpu=fpv4-sp-d16 -mfloat-abi=hard --static -nostartfiles -lnosys -Lbuild/release -Wl,--start-group -lc -lgcc -Wl,--end-group -labst_stm32f4 -lopencm3 -llist -Wl,--gc-sections -Wl,--print-gc-sections -specs=nano.specs -specs=nosys.specs
 SRCS = src/list.c \
 		   src/list_node.c \
 		   src/list_iterator.c
---
+++ src/list_iterator.d
@@ -0,0 +1,17 @@
+src/list_iterator.o: src/list_iterator.c src/list.h \
+ /usr/arm-none-eabi/include/stdlib.h \
+ /usr/arm-none-eabi/include/machine/ieeefp.h \
+ /usr/arm-none-eabi/include/_ansi.h /usr/arm-none-eabi/include/newlib.h \
+ /usr/arm-none-eabi/include/_newlib_version.h \
+ /usr/arm-none-eabi/include/sys/config.h \
+ /usr/arm-none-eabi/include/sys/features.h \
+ /usr/lib/gcc/arm-none-eabi/10.2.0/include/stddef.h \
+ /usr/arm-none-eabi/include/sys/reent.h \
+ /usr/arm-none-eabi/include/_ansi.h \
+ /usr/arm-none-eabi/include/sys/_types.h \
+ /usr/arm-none-eabi/include/machine/_types.h \
+ /usr/arm-none-eabi/include/machine/_default_types.h \
+ /usr/arm-none-eabi/include/sys/lock.h \
+ /usr/arm-none-eabi/include/sys/cdefs.h \
+ /usr/arm-none-eabi/include/machine/stdlib.h \
+ /usr/arm-none-eabi/include/alloca.h
